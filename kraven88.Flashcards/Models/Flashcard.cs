b71b629e470ce7a2ff434395668400db7b526923
kraven88 <k.rowecki@gmail.com>
2024-03-05 10:33:21 +0100

    Deleted files from other students projects

---
+++ kraven88.Flashcards/Models/Flashcard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kraven88.Flashcards.Models;
+internal class Flashcard
+{
+    public int Id { get; set; }
+    public int StackId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
